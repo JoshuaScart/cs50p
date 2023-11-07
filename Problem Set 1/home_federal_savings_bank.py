177c96b140f030008d5b3c0b00100ee0d2bec394
Joshua Scart <98558418+JoshuaScart@users.noreply.github.com>
2023-11-07 14:28:24 -0300

    Update home_federal_savings_bank.py

--- Problem Set 1/home_federal_savings_bank.py
+++ Problem Set 1/home_federal_savings_bank.py
@@ -2,10 +2,10 @@
 If the greeting starts with “hello”, output $0. If the greeting starts with an “h” (but not “hello”), output $20. 
 Otherwise, output $100. Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively. '''
 
-# Ask the user for the input:
+# Ask the user for the input
 answer=input("A customer arrives at you bank, how do you great him?")
 
-# Lowercase the input and remove any leading whitespaces:
+# Lowercase the input and remove any leading whitespaces
 answerf=answer.lower().strip()
 
 if answerf.startswith("hello"): # If the answer starts with "hello", print "$0".
