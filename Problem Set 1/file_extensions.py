3ee37af1d3ad348b7f4f9263dda55260c9b22224
Joshua Scart <98558418+JoshuaScart@users.noreply.github.com>
2023-11-07 14:23:45 -0300

    Update file_extensions.py

--- Problem Set 1/file_extensions.py
+++ Problem Set 1/file_extensions.py
@@ -1,4 +1,7 @@
-# In a file called extensions.py, implement a program that prompts the user for the name of a file and then outputs that file’s media type if the file’s name ends, case-insensitively, in any of these suffixes: .gif, .jpg, .jpeg, .png, .pdf, .txt, .zip. See developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types for common types.
+''' In a file called extensions.py, implement a program that prompts the user for the name of a file and then 
+outputs that file’s media type if the file’s name ends, case-insensitively, in any of these suffixes: 
+.gif, .jpg, .jpeg, .png, .pdf, .txt, .zip. 
+See developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types for common types. '''
 
 # Asks the user for input
 file = input("What's your file? ")
