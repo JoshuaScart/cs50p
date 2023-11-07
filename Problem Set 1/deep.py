3f47a812b8893398c2b2d5b44472855e1d6d9acf
Joshua Scart <98558418+JoshuaScart@users.noreply.github.com>
2023-11-07 14:24:27 -0300

    Update deep.py

--- Problem Set 1/deep.py
+++ Problem Set 1/deep.py
@@ -1,4 +1,5 @@
-# In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
+''' In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, 
+outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No. '''
 
 # Prompts the user for the answer:
 answer = input("What's the answer to The Great Question Of Life?")
