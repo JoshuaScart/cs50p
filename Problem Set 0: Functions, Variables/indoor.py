c57b8a31e3e348a931c32bdd309f76b5ce12ebe3
Joshua Scart <98558418+JoshuaScart@users.noreply.github.com>
2023-11-07 15:13:47 -0300

    Create indoor.py

---
+++ Problem Set 0: Functions, Variables/indoor.py
@@ -0,0 +1,9 @@
+'''In a file called indoor.py, implement a program in Python that prompts the user for input and then outputs that same input in lowercase. 
+Punctuation and whitespace should be outputted unchanged. 
+You’re welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input.'''
+
+# Ask the user for input
+uppercase_text = input("Insert uppercased text here: ")
+
+# Uppercase the input and print it
+print(uppercase_text.lower())
