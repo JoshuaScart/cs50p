1604b5091314c4088ad5cfbfe55c01ba79b03399
Joshua Scart <98558418+JoshuaScart@users.noreply.github.com>
2023-11-07 14:24:05 -0300

    Update home_federal_savings_bank.py

--- Problem Set 1/home_federal_savings_bank.py
+++ Problem Set 1/home_federal_savings_bank.py
@@ -1,4 +1,6 @@
-# In a file called bank.py, implement a program that prompts the user for a greeting. If the greeting starts with “hello”, output $0. If the greeting starts with an “h” (but not “hello”), output $20. Otherwise, output $100. Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
+''' In a file called bank.py, implement a program that prompts the user for a greeting. 
+If the greeting starts with “hello”, output $0. If the greeting starts with an “h” (but not “hello”), output $20. 
+Otherwise, output $100. Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively. '''
 
 # Ask the user for the input:
 answer=input("A customer arrives at you bank, how do you great him?")
